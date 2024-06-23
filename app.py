e1b8d06305171e9514a94c43cf23efaf99774bdd
Julio Rubén Sanic Martínez <julio.sanic.gt.256@gmail.com>
2024-06-22 21:34:14 -0600

    Recipe GET POST auth

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_restful import Api
 from config import Config
 from extensions import db, jwt
 from models.user import User
-from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
+from resources.recipe import RecipeListResource, RecipeResource 
 from resources.user import UserListResource, UserResource, MeResource
 from resources.token import TokenResource 
 
@@ -24,7 +24,7 @@ def register_resources(app):
     api = Api(app=app)
     api.add_resource(RecipeListResource, '/recipes')
     api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
-    api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
+    # api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
 
     api.add_resource(UserListResource, '/users')
     api.add_resource(UserResource, '/users/<string:username>')
