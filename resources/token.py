ff04614db2ba72b0f7660eccac619bfcc8d88cee
Julio Rubén Sanic Martínez <julio.sanic.gt.256@gmail.com>
2024-06-22 23:55:04 -0600

    refresh token

--- resources/token.py
+++ resources/token.py
@@ -18,7 +18,7 @@ class TokenResource(Resource):
         access_token = create_access_token(identity=user.id, fresh=True)
         refresh_token = create_refresh_token(identity=user.id)
 
-        return {'access_token': access_token}, HTTPStatus.OK
+        return {'access_token': access_token, 'refresh_token': refresh_token}, HTTPStatus.OK
     
 class RefreshResource(Resource):
     @jwt_required(refresh=True)
@@ -29,4 +29,4 @@ class RefreshResource(Resource):
 
         return {'access_token': access_token}, HTTPStatus.OK
     
-    
+
